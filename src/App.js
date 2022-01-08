9b55a0448048c38a9b6e3931c95d66745cea6500
AleMejias <alejandro_12_77@hotmail.com>
2022-01-08 19:39:48 -0300

    Modificacion en importaciones en el archivo App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { useEffect } from 'react';
-import { useState } from 'react/cjs/react.development';
+import React , { useState , useEffect } from 'react';
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
 import { useWidth } from './hooks/useWidth';
 
-function App() {
+const  App = () => {
   const [ themeMode , setThemeMode] = useState( false );
   const [ mobileView , setMobileView ] = useState( false );
   const [ widthScreen , setWidthScreen ] = useWidth();
